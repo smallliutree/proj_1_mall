cb89bfb9b4db55f18ba6f383d82f09abc78031b9
asd <asd@asd.com>
2020-08-21 12:06:34 +0800

    日志

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def index(request):
+
+    return HttpResponse('asd')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index)
 ]

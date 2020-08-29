151c116d50a551d26c3f6070dbb790b340b6ec52
asd <asd@asd.com>
2020-08-29 15:50:50 +0800

    查询市

--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -20,4 +20,22 @@ class AreasView(View):
 
 class SubAreasView(View):
     def get(self, request, parent_id):
-        pass
+        # subs = Area.objects.filter(parent=parent_id)
+        # 1
+        parent_area = Area.objects.get(id=parent_id)
+        subs = parent_area.subs.all()
+
+        sub_list = []
+        for item in subs:
+            sub_list.append({
+                'id': item.id,
+                'name': item.name
+            })
+
+        sub_data = {
+            'id': parent_area.id,
+            'name': parent_area.name,
+            'subs': sub_list
+        }
+
+        return JsonResponse({'code': 0, 'errmsg': 'OK', 'sub_data': sub_data})
